c581459082ada61bb5f1b0d1b8518f22e2395495
Eugene D <Eugene D>
2018-07-29 12:54:35 -0400

    HTML files are downloaded

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
    include "tilda-php/tilda-php-master/classes/Tilda/Api.php";
    include "tilda-php/tilda-php-master/classes/Tilda/LocalProject.php";
     use \Tilda;
@@ -8,20 +9,31 @@
     $api = new Tilda\Api(TILDA_PUBLIC_KEY, TILDA_SECRET_KEY);
     $local = new Tilda\LocalProject(array('projectDir' => 'tilda'));
     $local->setProject($api->getProjectExport(TILDA_PROJECT_ID));
+
     $local->createBaseFolders();
     $local->copyCssFiles('css');
     $local->copyJsFiles('js');
     $local->copyImagesFiles('img');
+    //echo json_encode($api->getPagesList(TILDA_PROJECT_ID), JSON_PRETTY_PRINT);
 
-   // $arFiles = $local->copyCssFiles('css');
-    /*
-    if (! $arFiles) {
-        die('Error in copy CSS files [' . $api->lastError . ']');
-    }
-    */
-    //print_r($arFiles);
-    //print_r($local->getProjectFullDir());
-    //print_r($api->getProjectsList());
+    $projectList = $api->getPagesList(TILDA_PROJECT_ID);
+    /*saving the html files*/
+    $increment = 0;
+    foreach($projectList as $mydata)
+    {
+         $increment = 0;
+         foreach($mydata as $values)
+         {  
+            if($increment == 0){
+                $local->savePage($api->getPage($values));
+            }
+            $increment++;
+         }
+    } 
+
+    //$local->savePage($api->getPage(2433918)); //TODO make loop to iterate for each page, this saves an html for the page id
+    //print_r($api->getPageExport(2433918)); //this does indeed show images
+
 ?>
 
 
