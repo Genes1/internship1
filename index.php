9d6e51ba28c54b60813582189b3576a0a835f53a
Eugene D <Eugene D>
2018-08-02 12:19:44 -0400

    Renamed deployer, added comments, added image download

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-
-   include "tilda-php/tilda-php-master/classes/Tilda/Api.php";
-   include "tilda-php/tilda-php-master/classes/Tilda/LocalProject.php";
+    //DECLARATION
+    include "tilda-php/tilda-php-master/classes/Tilda/Api.php";
+    include "tilda-php/tilda-php-master/classes/Tilda/LocalProject.php";
     use \Tilda;
+    ini_set("allow_url_fopen", true);                        //will this cause problems?
     define('TILDA_PUBLIC_KEY', '5ocb05o8cb32btmoyedg');
     define('TILDA_SECRET_KEY', 'hunr9ueg9woel0ql6fti');
     define('TILDA_PROJECT_ID', '577099');
@@ -10,14 +11,14 @@
     $local = new Tilda\LocalProject(array('projectDir' => 'tilda'));
     $local->setProject($api->getProjectExport(TILDA_PROJECT_ID));
 
+    //Attempt to create folders and copy css, js, and image files
     $local->createBaseFolders();
     $local->copyCssFiles('css');
     $local->copyJsFiles('js');
-    $local->copyImagesFiles('img');
-    //echo json_encode($api->getPagesList(TILDA_PROJECT_ID), JSON_PRETTY_PRINT);
+    $local->copyImagesFiles('img'); //the image copying fails, despite no error message. manual download is farther down
 
+    //saving the html files
     $projectList = $api->getPagesList(TILDA_PROJECT_ID);
-    /*saving the html files*/
     $increment = 0;
     foreach($projectList as $mydata)
     {
@@ -31,10 +32,18 @@
          }
     } 
 
-    //$local->savePage($api->getPage(2433918)); //TODO make loop to iterate for each page, this saves an html for the page id
-    //print_r($api->getPageExport(2433918)); //this does indeed show images
-
+    //saving the image files manually
+    $increment = 0;
+    foreach($api->getPagesList(TILDA_PROJECT_ID) as $page){
+        foreach($api->getPageExport($page["id"]) as $outerArrAcc)
+        {
+            if ($increment == 11){
+                foreach($outerArrAcc as $innerArr)
+                {     
+                    file_put_contents ('tilda\img\\'.$innerArr["to"], file_get_contents($innerArr["from"]) );
+                }
+            }
+            $increment++;
+        } 
+    }
 ?>
-
-
-
---
+++ local_deployer.bat
@@ -0,0 +1,5 @@
+xcopy /s /q /y "C:\MAMP\htdocs" "C:\Users\bankb_000\Desktop\htdocs backup"
+del /q "C:\MAMP\htdocs\*"
+for /d %%p in (C:\MAMP\htdocs\*) Do rd /Q /S "%%p"
+//cd "C:\Users\bankb_000\Desktop\internship repo"
+xcopy /s /q /y "C:\Users\bankb_000\Desktop\internship repo" "C:\MAMP\htdocs"
