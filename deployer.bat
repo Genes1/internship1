4810d6d71b3b3b2a3d1f80775d5e825fd4ae6ac4
Eugene Domrachev <bankblasta@gmail.com>
2018-08-31 03:01:05 -0400

    Delete deployer.bat

--- deployer.bat
+++
@@ -1,5 +0,0 @@
-xcopy /s /q /y "C:\MAMP\htdocs" "C:\Users\bankb_000\Desktop\htdocs backup"
-del /q "C:\MAMP\htdocs\*"
-for /d %%p in (C:\MAMP\htdocs\*) Do rd /Q /S "%%p"
-//cd "C:\Users\bankb_000\Desktop\internship repo"
-xcopy /s /q /y "C:\Users\bankb_000\Desktop\internship repo" "C:\MAMP\htdocs"
