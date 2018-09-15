3f24b26c855957233ee3506f867c54b8a9701d01
Eugene Domrachev <eug.domr@gmail.com>
2018-09-15 17:31:42 -0400

    Delete local_deployer.bat

--- local_deployer.bat
+++
@@ -1,4 +0,0 @@
-del /q "C:\MAMP\htdocs\*"
-for /d %%p in (C:\MAMP\htdocs\*) Do rd /Q /S "%%p"
-//cd "C:\Users\bankb_000\Desktop\internship repo"
-xcopy /s /q /y "C:\Users\bankb_000\Desktop\internship repo" "C:\MAMP\htdocs"
